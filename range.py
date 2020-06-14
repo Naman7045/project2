017eb4c7ff0818c939715ecd74f37d44fce1792f
nmnxo <66592970+nmnxo@users.noreply.github.com>
2020-06-14 18:00:48 +0530

    Add files via upload

---
+++ range.py
@@ -0,0 +1,18 @@
+list1 = [12, -7, 5, 64, -14] 
+
+for num in list1:  
+    if num >= 0: 
+	     print(num, end = " ")
+	 
+print(end='\n')
+
+list2 = [12, 14, -95, 3]
+
+print("[",end=" ")
+
+for num in list2:
+    if num >= 0:
+        print(num,end = " ")
+        
+ 
+print("]")
